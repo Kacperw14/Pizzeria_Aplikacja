38fbd092816017d0ebe12b2c89fdc98a08b98dc5
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2023-05-31 21:28:32 +0200

    Login z połączeniem się do bazy danych

--- app/src/main/java/com/example/pizzeria_aplikacja/Login.kt
+++ app/src/main/java/com/example/pizzeria_aplikacja/Login.kt
@@ -9,53 +9,73 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
 class Login : Fragment() {
+
+    private val driver = "oracle.jdbc.driver.OracleDriver"
+    private val url = "jdbc:oracle:thin:@192.168.0.38:1521:orcl"
+    private val user = "\"KELNER\""
+    private val password = "\"1234\""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
         val button = view.findViewById<Button>(R.id.button3)
-        button.setOnClickListener{
+        button.setOnClickListener {
             findNavController().navigate(R.id.action_login_to_rejestracja)
         }
         return view
     }
 
+    private fun getConnection(): Connection? {
+        var connection: Connection? = null
+        Class.forName(driver)
+        try {
+            connection = DriverManager.getConnection(url, user, password)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+        }
+        return connection
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val buttonLogin = view.findViewById<Button>(R.id.buttonLogin)
         buttonLogin.setOnClickListener {
-            // Handle login button click event
             loginUser()
         }
     }
 
     private fun loginUser() {
-        // Perform login logic here
 
-        val editTextUsername = view?.findViewById<EditText>(R.id.editTextUsername)
-        val editTextPassword = view?.findViewById<EditText>(R.id.editTextPassword)
+        val email = view?.findViewById<EditText>(R.id.editTextUsername)?.text.toString()
+        val haslo = view?.findViewById<EditText>(R.id.editTextPassword)?.text.toString()
 
-        val username = editTextUsername?.text.toString()
-        val password = editTextPassword?.text.toString()
 
         // Example login logic
-        if (username == "admin" && password == "p") {
-            findNavController().navigate(R.id.action_login_to_rejestracja)
-            // Login success, navigate to next screen
-            // Replace the code below with your own logic
-            Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
+        //if (email == "admin" && haslo == "p") {
+        if (true) {
+            val connection: Connection?
+            try {
+                connection = getConnection()
+                if (connection != null) {
+                    Toast.makeText(requireContext(), "Connected", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_login_to_rejestracja)
+                } else Toast.makeText(requireContext(), "Login failed", Toast.LENGTH_SHORT).show()
+                connection?.close()
+            } catch (e: SQLException) {
+                Toast.makeText(requireContext(), e.toString(), Toast.LENGTH_SHORT).show()
+            }
         } else {
-            // Login failed, show error message
-            // Replace the code below with your own logic
             Toast.makeText(requireContext(), "Login failed", Toast.LENGTH_SHORT).show()
         }
     }
-
 }
 
--- app/src/main/java/com/example/pizzeria_aplikacja/MainActivity.kt
+++ app/src/main/java/com/example/pizzeria_aplikacja/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.pizzeria_aplikacja
 
+import android.content.Context
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.SQLException
 
 class MainActivity : AppCompatActivity() {
+
+
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val threadPolicy: StrictMode.ThreadPolicy =
+            StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(threadPolicy)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         navController = navHostFragment.navController
     }
+
+
 }
